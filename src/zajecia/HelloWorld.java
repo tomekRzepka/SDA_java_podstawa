2b0bde1f627a2ac45edda0228b0f662169863896
unknown <rzepka.tomek@gmail.com>
2017-04-06 20:44:13 +0200

    initial commit

---
+++ src/zajecia/HelloWorld.java
@@ -0,0 +1,10 @@
+package zajecia;
+
+/**
+ * Created by RENT on 2017-04-06.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
