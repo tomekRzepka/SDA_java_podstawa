b4e53f6aa766f34ff06b1b19d423e4c1bcda59e5
unknown <rzepka.tomek@gmail.com>
2017-04-14 21:01:07 +0200

    Added method on strings

--- src/zajecia/zajecia8.java
+++ src/zajecia/zajecia8.java
@@ -5,11 +5,123 @@ package zajecia;
  */
 public class zajecia8 {
     public static void main(String[] args) {
-        String message = "Ala ma kota";
+//        String message = "Ala ma kota";
 //        System.out.println(countCharInText(message,'a'));
-        System.out.println(countWords("Ala ma kota a kot ma AIDS"));
-        System.out.println(switchCase("AbCdEfGh"));
+//        System.out.println(countWords("Ala ma kota a kot ma AIDS"));
+//        System.out.println(switchCase("AbCdEfGh"));
+      //  System.out.println(sumOfNumbers("Ala ma 2 koty i 3 psy"));
+        System.out.println(cezarCode("ABCDE EF"));
+
     }
+    // // TODO: 2017-04-14 nie kodowc spacji i z  na a
+    //*****************A->B B->C**************************************
+    public static String cezarCode(String messageToCode,int offset){
+        return cezarCoder(messageToCode,offset);
+    }
+
+    //*****************A<-B B<-C**************************************
+    public static String cezarDecode(String coddedMessage,int offset){
+        return cezarCoder(coddedMessage,-offset);
+    }
+
+
+    //*****************A->B B->C**************************************
+    public static String cezarCode(String messageToCode){
+        return cezarCoder(messageToCode,1);
+    }
+
+    //*****************A<-B B<-C**************************************
+    public static String cezarDecode(String coddedMessage){
+        return cezarCoder(coddedMessage,-1);
+    }
+
+    //*****************A->B B->C**************************************
+    private static String cezarCoder(String message,int offset){
+        char [] charArray = message.toCharArray();
+
+        for (int i = 0; i <charArray.length ; i++) {
+            charArray[i]+=offset;
+ //           charArray[i]+=(charArray[i]==97||charArray[i]==65)?25:(charArray[i]==90||charArray[i]==122)?-25:-1;
+
+        }
+        return String.valueOf(charArray);
+    }
+
+    // substring ("Ala ma kota", 4,6) --> ma
+    //*************************************************************************
+//    public static String subString(String message){
+//
+//    }
+
+
+
+    //*************************************************************************
+    public static int sumOfNumbers(String message){
+
+        char[] charArray= message.toCharArray();
+        int sum=0;
+
+        for (int i = 0; i <charArray.length ; i++) {
+            if(charArray[i]>48&&charArray[i]<=57 ){
+                sum+=charArray[i]-48;
+            //   sum+=Character.getNumericValue(charArray[i]);
+
+            }
+
+        }
+
+//        for (int i = 0; i <message.length() ; i++) {
+//
+//            if (message.charAt(i) >= 48 && message.charAt(i) <= 57){
+//
+//            }
+//        }
+
+        return sum;
+    }
+
+    //*************************************************************************
+    public static String toLowerCase(String message){
+        char [] charArray = message.toCharArray();
+        if(charArray[0] >= 65 && charArray[0]<=90){
+            charArray[0]+=32;
+        }
+        //  charArray[0]-= (charArray[0] >=65 && charArray[0]<=90)? 32 : 0;
+        return String.valueOf(charArray);
+    }
+    //*************************************************************************
+    public static String toUpperCase(String message){
+        char [] charArray = message.toCharArray();
+        if(charArray[0] >= 97 && charArray[0]<=122){
+            charArray[0]-=32;
+        }
+      //  charArray[0]-= (charArray[0] >= 97 && charArray[0]<=122)? 32 : 0;
+        return String.valueOf(charArray);
+    }
+
+
+    //************************************************************************
+    public static boolean startsWith(String message, String someValue){
+
+        char [] messageCharacters=message.toCharArray();
+        char [] someValueCharacters= someValue.toCharArray();
+        boolean flag=true;
+        int i=0;
+        while (flag&& i<someValueCharacters.length){
+            if(someValueCharacters[i]!=messageCharacters[i]){
+                flag=false;
+            }
+
+        i++;
+//            //
+//           if( message.charAt(i) == someValue.charAt(i)){
+//
+//
+        }
+        return flag ;
+    }
+
+
     //**********************************************************************
     public static boolean isPalindrome(String message){
         char [] charArray=message.toCharArray();
