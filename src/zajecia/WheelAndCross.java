e1d69f4c5dc076ae17d942d0f8f756199148b6cd
unknown <rzepka.tomek@gmail.com>
2017-04-21 18:47:35 +0200

    Added T&O

--- src/zajecia/WheelAndCross.java
+++ src/zajecia/WheelAndCross.java
@@ -7,28 +7,98 @@ import java.util.Scanner;
  */
 public class WheelAndCross {
     public static void main(String[] args) {
-        char [][]gameBoard={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-        Scanner scanner=new Scanner(System.in);
-        displayBoard(gameBoard);
-        for (int i = 0; i <9 ; i++) {
+        char[][] gameBoard = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+        Scanner scanner = new Scanner(System.in);
+        int i=0;
+        while (!checkIsGameEnded(gameBoard,i)){
 
-            System.out.println("insert sign position");
-            int position=scanner.nextInt();
-            int[]positions=convertPosition(position);
-            gameBoard[positions[0]][positions[1]]=i%2==0?'X':'O';
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-
+            displayBoard(gameBoard);
+            int position = getPositionFromUser(gameBoard);
+            int [] positions = convertPosition(position);
+            gameBoard[positions[0]][positions[1]] = i % 2 == 0 ? 'X' : 'O';
+            i++;
         }
         System.out.println("End of game");
         displayBoard(gameBoard);
 
     }
+
+    public static int getPositionFromUser(char[][] gameBoard) {
+        Scanner scanner =new Scanner(System.in) ; int i =0 ; boolean inputFromUserFlag =false ;
+        int position = 0;
+        while (!inputFromUserFlag) {
+
+
+            System.out.println("insert sign position");
+             position = scanner.nextInt();
+            inputFromUserFlag = validatePositionFromUser(position, gameBoard);
+            if (!inputFromUserFlag) {
+                System.out.println("invalid date");
+            }
+        }
+        return position;
+    }
+
+    public static boolean checkIsGameEnded(char[][] gameBoard , int gameStep) {
+        return  checkRows(gameBoard) || checkColumns(gameBoard) || checkDiagonals(gameBoard)|| !(gameStep< 9) ;
+
+    }
+
+    public static boolean checkRows(char[][] gameBoard) {
+        boolean end = false;
+        int i = 0;
+        while (!end && i < gameBoard.length) {
+            end = gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][1] == gameBoard[i][2];
+
+            i++;
+        }
+        return end;
+
+    }
+    public static boolean checkColumns(char [][] gameBoard){
+        boolean end=false;
+        int i=0;
+        while(!end&&i<gameBoard.length){
+            if(gameBoard[0][i]==gameBoard[1][i]&&gameBoard[1][i]==gameBoard[2][i]){
+                end = true;
+            }
+            i++;
+        }
+        return end;
+
+
+    }
+    public static boolean checkDiagonals(char [][] gameBoard){
+        return !isFieldEmpty(gameBoard,1,1)&&(checkFirstDiagonal(gameBoard)||checkFirstSecond(gameBoard))
+//        boolean end=false;
+//        int i=0;
+//        while(i<gameBoard.length && end==false){
+//            if(gameBoard[0][0]==gameBoard[1][1]&&gameBoard[1][1]==gameBoard[2][2]||gameBoard[2][0]==gameBoard[1][1]&&gameBoard[1][1]==gameBoard[0][2]){
+//                end = true;
+//            }
+//            i++;
+//
+//        }
+//        return end;
+
+    }
+
+    public static boolean checkFirstDiagonal(char[][]gameBoard){
+        return (gameBoard[0][0]==gameBoard[1][1]&&gameBoard[0][0]==gameBoard[2][2]);
+    }
+    public static boolean checkFirstSecond(char[][]gameBoard){
+        return (gameBoard[0][2]==gameBoard[1][1]&&gameBoard[0][0]==gameBoard[2][0]);
+    }
+
+    public static boolean validatePositionFromUser(int positionFromUser, char[][]gameboard){
+        int[] ints =convertPosition(positionFromUser);
+        return(positionFromUser>0&&positionFromUser<10&&isFielempty(gameboard,ints[0],ints [1]));
+    }
+    public static boolean isFieldEmpty(char[][]gameBoard,int i, int j){
+        return !(gameBoard[i][j]==)
+    }
+
+
     public static int [] convertPosition(int positionFromUser){
         int [] position = new int[2];
         position[0]=(positionFromUser-1)/3;
